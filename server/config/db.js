09376fa0282b218c9a5c4c1e2251982056728fb8
Kavindu Sandeepa Amaranath <kavindusandeepa20031030@gmail.com>
2026-06-04 23:56:27 +0530

    improve db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const connectDB = async () => {
-    await mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB Connected"))
-    .catch((error) => console.log(error));
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log("MongoDB Connected Successfully");
+    } catch (error) {
+        console.error("MongoDB Connection Failed:", error.message);
+        process.exit(1);
+    }
 };
