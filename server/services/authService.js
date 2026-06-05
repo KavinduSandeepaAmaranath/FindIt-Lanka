2fb198efbcd64e98e54a9aecbcd9c6eba64592cd
Kavindu Sandeepa Amaranath <kavindusandeepa20031030@gmail.com>
2026-06-05 13:27:15 +0530

    edit authService.js

--- server/services/authService.js
+++ server/services/authService.js
@@ -4,22 +4,43 @@ import jwt from "jsonwebtoken";
 import { generateAccessToken } from "../utils/generateAccessToken.js";
 import { generateRefreshToken } from "../utils/generateRefreshToken.js";
 
-export const registerUser = async ({ name, email, phoneNumber, district, password, confirmPassword }) => {
-    try {
-        if (password !== confirmPassword) {
-            throw new Error("Password does not match");
-        }
+export const registerUser = async ({ 
+    name, 
+    email, 
+    phoneNumber, 
+    district, 
+    password, 
+    confirmPassword 
+}) => {
 
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            throw new Error("User already exists");
-        }        
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        return await User.create({ name, email, phoneNumber, district, password: hashedPassword });
-        
-    } catch (error) {
-        throw error;
+    if (password !== confirmPassword) {
+        throw new Error("Password does not match");
     }
+
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,64}$/;
+
+    if (!passwordRegex.test(password)) {
+        throw new Error(
+            "Password must be 8-64 characters long and include at least one uppercase letter, one lowercase letter, and one number"
+        );
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+        throw new Error("User already exists");
+    }        
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({ 
+        name, 
+        email, 
+        phoneNumber, 
+        district, 
+        password: hashedPassword 
+    });
+
+    return user;
+
 };
