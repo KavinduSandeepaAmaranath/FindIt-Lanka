1eb0eeab9f42353e9abf38ddef2980f209d53f78
Kavindu Sandeepa Amaranath <kavindusandeepa20031030@gmail.com>
2026-06-07 22:30:48 +0530

    modify authController.js

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,4 +1,33 @@
-import { registerUser } from "../services/authService.js";
+import { sendOTP, verifyOTP, registerUser } from "../services/authService.js";
+
+export const sendOTPController = async (req, res) => {
+    try {
+        const { email } = req.body;
+
+        const result = await sendOTP(email);
+
+        res.status(200).json(result);
+
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+        });        
+    }
+};
+
+export const verifyOTPController = async (req, res) => {
+    try {
+        const { email, otp } = req.body;
+
+        const result = await verifyOTP(email, otp);
+
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message,
+        });
+    }
+};
 
 export const register = async (req, res) => {
     try {
